4b6d9ccb643f48609d6b8fa199dad0a462c77631
ggarl <leejj01@gmail.com>
2018-01-10 13:33:52 +0000

    ioConnect

--- client/js/game.js
+++ client/js/game.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://worldending-ggarl.c9users.io/',{reconnect:true}); /* glabal io $*/
+var socket = io.connect(process.env.PORT); /* glabal io $*/
 
 socket.on('message', function(data) {
     console.log(data);
